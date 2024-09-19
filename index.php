f5773c90799bc37d5e575bca6fb8fe4deda02191
sanskar1215 <112830708+sanskar1215@users.noreply.github.com>
2024-09-19 10:12:10 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    
+    <title>Home Page - Online Birth Certificate System</title>
+
+
+
+    <link href="style.css" rel="stylesheet">
+
+    
+    <link href="css/responsive/responsive.css" rel="stylesheet">
+
+</head>
+
+<body>
+ 
+    <div id="preloader">
+        <div class="loader">
+            <span class="inner1"></span>
+            <span class="inner2"></span>
+            <span class="inner3"></span>
+        </div>
+    </div>
+
+   
+
+  
+    <header class="header_area" id="header">
+        <div class="container-fluid h-100">
+            <div class="row h-100">
+                <div class="col-12 h-100">
+                    <nav class="h-100 navbar navbar-expand-lg align-items-center">
+                        <a class="navbar-brand" href="index.php">OBCS</a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#fancyNav" aria-controls="fancyNav" aria-expanded="false" aria-label="Toggle navigation"><span class="ti-menu"></span></button>
+                        <div class="collapse navbar-collapse" id="fancyNav">
+                            <ul class="navbar-nav ml-auto">
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+                                </li>
+                             
+                                <li class="nav-item">
+                                    <a class="nav-link" href="user/login.php">User</a>
+                                </li>
+                                                                <li class="nav-item">
+                                    <a class="nav-link" href="user/search.php">Verify Certifcate</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="admin/login.php">Admin</a>
+                                </li>
+                             
+                            </ul>
+                         
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+
+
+    <div class="fancy-hero-area bg-img bg-overlay animated-img" style="background-image: url(img/obcs.png);">
+       
+    </div>
+    <footer class="fancy-footer-area fancy-bg-dark">
+      
+        <div class="footer-copywrite-area">
+            <div class="container h-100">
+                <div class="row h-100">
+                    <div class="col-12 h-100">
+                        <div class="copywrite-content h-100 d-flex align-items-center justify-content-between">
+
+                            <div class="copywrite-text">
+                                <p>Online Birth Certificate System </p>
+                            </div>
+                            <div class="footer-nav">
+                                <nav>
+                                    <ul>
+                                        <li><a href="index.php">Home</a></li>
+                                        <li><a href="admin/login.php">Admin</a></li>
+                                        <li><a href="user/login.php">User</a></li>
+                                      
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script src="js/jquery/jquery-2.2.4.min.js"></script>
+    <script src="js/bootstrap/popper.min.js"></script>
+    <script src="js/bootstrap/bootstrap.min.js"></script>
+    <script src="js/others/plugins.js"></script>
+    <script src="js/active.js"></script>
+</body>
